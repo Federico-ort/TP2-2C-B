3e966a0e41d643b6624f9cf757bd6dbab8474783
FedericoLoterstein <federicofotografias@gmail.com>
2020-09-09 13:55:51 -0300

    balancedBrackeds Completo

--- basic/ejercicios/balancedBrackeds.js
+++ basic/ejercicios/balancedBrackeds.js
@@ -1,7 +1,28 @@
 // Complete the isBalanced function below.
 function isBalanced(s) {
-
-
+    //creo dos arrays (apertura) para guardar { [ ( y cierre para guardar )]}
+    let apertura = [];
+    let cierre = [];
+ //ciclo FOR para recorrer la cadena de texto que recibo por parametro
+    for (let index = 0; index < s.length; index++) {
+        //Verifico a que array corresponde
+        switch (s[index]) {
+            case '(':
+                apertura.push(s[index]);
+                break;
+            case '{':
+                apertura.push(s[index]);
+                break;
+            case '[':
+                apertura.push(s[index]);
+                break;
+            default:
+                cierre.push(s[index]);
+                break;
+        }
+    }
+    // antes de retornar verifico si estan equilibradas las dos cadenas. 
+    return apertura.length === cierre.length ? 'YES' : 'NO';
 }
 
 // TESTS
